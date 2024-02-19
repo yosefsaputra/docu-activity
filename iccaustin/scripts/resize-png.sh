7adbbb3c2d3bd99f1b7e56268c3c02371a671c95
Yosef Saputra <yosef.saputra@gmail.com>
2024-02-19 13:31:45 -0600

    improve sharpness

--- iccaustin/scripts/resize-png.sh
+++ iccaustin/scripts/resize-png.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-files=(ps-347b.png ps-348b.png ps-390b.png ps-411b.png ps-322.png ps-592.png ps-552a.png ps-524.png)
+files=(ps-322.png ps-592.png ps-552a.png ps-524.png)
 dir=../musicsheets
 
 function resize() {
   fileName=$1
-  convert $fileName -resize 992 $fileName
+  convert $fileName -resize 992 -density 240 -sharpen 0x1.0 -colorspace RGB -background white -alpha remove -alpha off -quality 100 $fileName 
 }
 
 for file in "${files[@]}"; do
